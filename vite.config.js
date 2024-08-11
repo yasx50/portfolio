c0a1cbf024a0c866ae2d8f9d89f3252714c9f29d
yasx50 <yasxcode@gmail.com>
2024-08-11 11:43:01 +0530

    VITE CONFIG

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
+  },
+  preview: {
     host: '0.0.0.0',
-    port: 3000, // You can choose any available port
+    port: process.env.PORT || 3000,
   },
 });

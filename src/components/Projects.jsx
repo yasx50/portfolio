484d9b56c9eeda78b98093ef25e91c88076c5476
yasx50 <yasxcode@gmail.com>
2024-09-03 21:10:49 +0530

    atv

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,9 +11,9 @@ const Projects = () => {
         <div className="w-full min-h-screen bg-black text-purple-300  border-white border-1">
           <h1 className="text-4xl p-10 border-1 block animate-pulse "><b>Projects</b></h1>
           <div className="all border-1 flex flex-wrap items-start justify-center gap-4 mt-5 ">
-          <div className="AudioToVideo border-2 p-5 rounded-2xl max-w-xs md:max-w-sm">
+          <div className="VideoToAudio border-2 p-5 rounded-2xl max-w-xs md:max-w-sm">
                 <h2 className="text-yellow-400 text-center text-2xl mt-5 p-3 bg-zinc-800 border-2 rounded-md">
-                  Audio To Video
+                   Video To Audio
                 </h2>
                 <div className="image flex flex-wrap justify-center mt-4">
                   {/* <img src={image} alt="" /> */}

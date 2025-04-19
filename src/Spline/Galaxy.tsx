320b474e32b458acfb9aa5e8e168219815bb3b61
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-04-19 19:03:27 +0530

    Spline Background Completed

--- src/Spline/Galaxy.tsx
+++ src/Spline/Galaxy.tsx
@@ -2,7 +2,7 @@ import Spline from '@splinetool/react-spline';
 
 export default function Galaxy() {
   return (
-    <div className="fixed h-full w-full top-0 -z-10">
+    <div className="fixed h-[120%] w-full top-0">
       <Spline scene="https://prod.spline.design/Xis7KV-PObUcmc4O/scene.splinecode" />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return (
         <nav className="container mx-auto p-8 border-2 border-white">
-
+            
         </nav>
     )
 }

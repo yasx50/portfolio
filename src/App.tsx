ae90c657bd3e9522b9eb7fb7ab2ffe86d9dc8dc6
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-04-22 22:18:44 +0530

    Add lenis and CSS to remove scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Layout from "./Layouts/Layout"
 import Home from "./components/Home"
+import { useEffect } from "react"
+import Lenis from 'lenis'
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    lenis.on("scroll", (e) => {
+      console.log(e);
+    })
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, [])
 
   return (
     <BrowserRouter>

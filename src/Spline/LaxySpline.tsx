ebf8b3470b273d6d635d6f9cfa43f10dc34fa2f0
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-04-24 23:33:29 +0530

    Video visiiblity solve

--- src/Spline/LaxySpline.tsx
+++ src/Spline/LaxySpline.tsx
@@ -5,7 +5,7 @@ const Galaxy = lazy(() => import('./Galaxy'))
 export default function LazySpline() {
     return (
         <>
-            <video autoPlay loop className='fixed w-full -top-18 -z-10' muted src='/spline_2.mp4' />
+            <video autoPlay loop className='fixed w-full -top-18 -z-10 border h-[120%] object-cover' muted src='/spline_2.mp4' />
             <Suspense fallback={<div className="text-white text-center text-[.5rem]">Loading 3D Scene...</div>}>
                 <Galaxy />
             </Suspense>
